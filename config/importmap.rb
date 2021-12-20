9307d126315b71ca8be93f901cc2b659541e01b2
Edward Rene Acosta <Eacosta2@satx.rr.com>
2021-12-19 18:24:34 -0600

    added localized times and blog formatting

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "local-time" # @2.1.0
+"https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
+
