052f68198a147a88eae57e4e1014bd75768337c6
Edward Rene Acosta <Eacosta2@satx.rr.com>
2021-12-24 08:21:01 -0600

    corrections finally made

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
-end
+    before_action :set_post
+
+    def create
+        @post.comments.create! params.required(:comment).permit(:content)
+        redirect_to @post  
+
+    end
+    
+    private 
+        def set_post    
+            @post = Post.find(params[:post_id])
+
+        end
+              
+    end
